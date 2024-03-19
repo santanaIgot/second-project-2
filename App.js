1b128101ffb04bd2919789787bb4213a4557a768
satanaIgot <santanaigoor01@gmail.com>
2024-03-18 22:45:17 -0300

    Feat - calc of distance

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Modal, StyleSheet, Text, View } from 'react-native';
+import { Modal, StyleSheet, Text, View,TextInput, Button } from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
 
 export default function App() {
 
   const[modalVisible, setModalVisible] = useState(false)
+  const [speed, setSpeed] = useState(0)
+  const [time, setTime] = useState(0)
+  const [distance, setDistance] = useState(0);
 
+
+
+  function calcOfDistance() {
+    setDistance(speed * time)
+  }
 
   return (
     <View style={styles.container}>
@@ -18,6 +26,32 @@ export default function App() {
       >
           <View style={[styles.container, {backgroundColor :'lightseagreen'}]}>
               <Text>Exemplo modal</Text>
+              <Text>Time</Text>
+                <TextInput
+                placeholder="set your time in hours"
+                style={styles.input}
+                value={time}
+                placeholderTextColor='#999'
+                onChangeText={(e) => setTime(e)}
+                />
+
+          <Text>Speed:</Text>
+                <TextInput
+                style={styles.input}
+                placeholder="set your speed in KM"
+                value={speed}
+                placeholderTextColor='#999'
+                onChangeText={(e) => setSpeed(e)}
+                />
+
+              <Button
+                      style={styles.button}
+                      title = 'Calc'
+                      onPress={()=>calcOfDistance()}
+                    />
+
+
+<Text style={styles.button}>Distance: {distance}</Text>
               <TouchableOpacity
                 onPress={()=> setModalVisible(!modalVisible)}>
                 <Text>
